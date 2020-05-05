f951fcd61ab6a22f1d305d0073d0188f3c87c23c
anmarkosyan <anush.markos@gmail.com>
2020-05-05 15:52:49 +0400

    second commit

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,12 +1,13 @@
 function numberCompare (arr){
-    //approach #1 ES5
+    //approach #2 ES6
+    const swap = (arr,num1,num2) => {
+        [arr[num1],arr[num2]] = [arr[num2],arr[num1]]
+
+    };
     for(let i = arr.length; i > 0; i--){
-        for(let j = 0; j < i -1; j++){
-            if(arr[j] > arr[j + 1]){
-                //SWAP!
-                let swap= arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] =swap;
+        for(let j = 0; j < i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr,j,j+1);
             }
         }
     }
