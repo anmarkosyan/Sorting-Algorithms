e2b1b07463585129b1165ee95b7079621bc84c46
anmarkosyan <anush.markos@gmail.com>
2020-05-11 12:04:24 +0400

    using ES6

--- insertionSort.js
+++ insertionSort.js
@@ -3,36 +3,27 @@
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+ const insertionSort = arr => {
+ const len = arr.length;
+     for(let i = 0; i < len; i++){
+         let el = arr[i];
+         let j;
+         for(j = i -1; j >=0 && arr[j] > el; j--){
+             arr[j + 1] = arr[j];
+         }
+         arr[j + 1] = el;
 
-function insertionSort (arr){
-    //loop over the array, stating with second element
-    for(let i = 1; i <arr.length; i++){
-        let currVal = arr[i];
-        //loop for go backward  and comparing  last and -1 element
-        for(var j = i - 1; j >= 0 && arr[j] > currVal; j--){
-            arr[j+1] = arr[j];
-        }
-       arr[j+1] = currVal;
-    }
+     }
      return arr;
+
+
 }
 
 console.log(insertionSort([1,5,2,8,56,24]));//[1,2,5,8,24,56]
 console.log(insertionSort([4,1,3,2,13,10]));//[1,2,3,4,10,13]
+console.log(insertionSort([4,2,1]))//[1,2,4]
 
 
-/*function insertionSort(arr){
-    var currentVal;
-    for(var i = 1; i < arr.length; i++){
-        currentVal = arr[i];
-        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-            arr[j+1] = arr[j]
-        }
-        arr[j+1] = currentVal;
-    }
-    return arr;
-}
 
-insertionSort([2,1,9,76,4])
 
- */
+
