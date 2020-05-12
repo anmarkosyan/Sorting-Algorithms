dd913abecc3cf62c8b41473919431a31db20a2a4
anmarkosyan <anush.markos@gmail.com>
2020-05-12 14:50:01 +0400

    adding some details

--- quickSort.js
+++ quickSort.js
@@ -1,9 +1,6 @@
 
-//***************************** quickSort_pivot ************************
-//write a function to sort a list of elements using Quick sort,and
-//return the index of pivot element,its not matter order ,only matter
-//the correct spot of pivot number,anything in the left side should
-//be less than pivot element, and the right side greater
+//***************************** quickSort ************************
+//Write a function to sort a list of elements using Quick sort.
 //****************************************************************
 
 //quickSort([6,9,4,1,7,8,2,10])//3 => [4,1,2,6,9,7,8,10] 6 is our pivot element,so we
@@ -11,7 +8,12 @@
 //left side,others put in right side ,there are  greater than 6 ,and
 //finally the position of pivot element (6) will be 3.
 
+
+// In the first function return the index of pivot element,its not matter order ,only matter
+//the correct spot of pivot number,anything in the left side should
+//be less than pivot element, and the right side greater
 function pivot(arr, start = 0, end = arr.length - 1){
+    //function for swapping elements
     function swap(arr,i,j){
         let temp = arr[i];
         arr[i] = arr[j];
@@ -31,6 +33,9 @@ function pivot(arr, start = 0, end = arr.length - 1){
 }
 console.log(pivot([6,9,4,1,7,8,2,10]));//3
 console.log(pivot([8,4,2,13,24,56,1,5]))//4
+
+
+//create a function that will be recursively sort the elements in the array
 function quickSort (arr, left = 0, right = arr.length -1){
     if(left < right) {
         let pivotIdx = pivot(arr, left, right);
